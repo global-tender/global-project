7cdcb17f35ec26a4425dbfed4f1f9df29e65f1b3
Igor Popov <ihptru@gmail.com>
2015-08-31 10:32:47 +0000

    SEO changes, other steps to begin

---
+++ contacts/index.php
@@ -0,0 +1,11 @@
+<?php
+    global $page_title;
+    $page_title = "Контакты";
+    include_once("../header.php");
+?>
+
+<p>Contacts.</p>
+
+<?php
+    include_once("/footer.php");
+?>
---
+++ projects/index.php
@@ -0,0 +1,11 @@
+<?php
+    global $page_title;
+    $page_title = "Проекты";
+    include_once("../header.php");
+?>
+
+<p>Projects.</p>
+
+<?php
+    include_once("/footer.php");
+?>
