43c39f95f169e2e344bcb38a6dcff27802e67884
Igor Popov <ihptru@gmail.com>
2015-09-17 17:41:44 +0000

    yandex metrika

--- footer.php
+++ footer.php
@@ -71,5 +71,32 @@
 			
 		</script>
 		<!-- //Yandex.Map -->
+
+		<!-- Yandex.Metrika counter -->
+		<script type="text/javascript">
+		(function (d, w, c) {
+		(w[c] = w[c] || []).push(function() {
+		try {
+		w.yaCounter27745992 = new Ya.Metrika({id:27745992,
+		webvisor:true,
+		clickmap:true,
+		trackLinks:true,
+		accurateTrackBounce:true});
+		} catch(e) { }
+		});
+
+		var n = d.getElementsByTagName("script")[0],
+		s = d.createElement("script"),
+		f = function () { n.parentNode.insertBefore(s, n); };
+		s.type = "text/javascript";
+		s.async = true;
+		s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+		if (w.opera == "[object Opera]") {
+		d.addEventListener("DOMContentLoaded", f, false);
+		} else { f(); }
+		})(document, window, "yandex_metrika_callbacks");
+		</script>
+		<!-- /Yandex.Metrika counter -->
 	</body>
 </html>
