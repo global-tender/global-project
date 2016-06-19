748b01bbf7cfd88311fc94448f7fce2ed3504c50
ihptru <ihptru@gmail.com>
2016-06-19 20:26:50 +0300

    style change

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
 	<header class="head-menu">
 		<nav>
 			<ul class="menu-ul">
-				<li class="menu-li"><a href="/">Главная</a></li>
 				<li class="menu-li"><a href="#" class="projects-menu">Проекты</a></li>
 				<li class="menu-li"><a href="#" class="contacts">Контакты</a></li>
 				<li class="menu-li ask-question"><a href="#" title="Задать вопрос" onclick="return showForm();">Задать вопрос</a></li>
