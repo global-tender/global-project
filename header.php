4757cef369b1dc1e7490b4c07febd456652c0afd
ihptru <ihptru@gmail.com>
2016-11-02 19:19:00 +0300

    first-project-free-text

--- header.php
+++ header.php
@@ -36,7 +36,7 @@
 
 		<link rel="stylesheet" href="/css/normalize.css">
 		<link rel="stylesheet" href="/css/main.css">
-		<link rel="stylesheet" href="/css/style.css?ver=0.007">
+		<link rel="stylesheet" href="/css/style.css?ver=0.008">
 
 	</head>
 
