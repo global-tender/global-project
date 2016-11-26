d079c1e91d74e2a9067e2383d2b02568a2a50686
ihptru <ihptru@gmail.com>
2016-11-26 20:55:48 +0300

    fix google code

--- header.php
+++ header.php
@@ -40,11 +40,7 @@
 
 		<!-- Google -->
 		<script type="text/javascript">
-		(function(a,e,c,f,g,h,b,d){var  
-		k={ak:"867216113",cl:"mUUDCLSmoWwQ8dXCnQM",autoreplace:"+7 950 869 35  
-		01"};a[c]=a[c]||function(){(a[c].q=a[c].q||[]).push(arguments)};a[g]|| 
-		(a[g]=k.ak);b=e.createElement(h);b.async=1;b.src="//www.gstatic.com/wcm/loader.js";d=e.getElementsByTagName(h)[0];d.parentNode.insertBefore(b,d);a[f]=function(b,d,e){a[c](2,b,k,d,null,new  
-		Date,e)};a[f]()})(window,document,"_googWcmImpl","_googWcmGet","_googWcmAk","script");
+			(function(a,e,c,f,g,h,b,d){var k={ak:"867216113",cl:"mUUDCLSmoWwQ8dXCnQM",autoreplace:"+7 950 869 35 01"};a[c]=a[c]||function(){(a[c].q=a[c].q||[]).push(arguments)};a[g]|| (a[g]=k.ak);b=e.createElement(h);b.async=1;b.src="//www.gstatic.com/wcm/loader.js";d=e.getElementsByTagName(h)[0];d.parentNode.insertBefore(b,d);a[f]=function(b,d,e){a[c](2,b,k,d,null,new Date,e)};a[f]()})(window,document,"_googWcmImpl","_googWcmGet","_googWcmAk","script");
 		</script>
 
 	</head>
