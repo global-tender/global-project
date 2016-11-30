a56e3073aef7a3906d85d3dcca88b2d42e7ba159
ihptru <ihptru@gmail.com>
2016-11-30 21:13:59 +0300

    ремаркетинг google

--- footer.php
+++ footer.php
@@ -89,5 +89,24 @@
 	</script>
 	<noscript><div><img src="https://mc.yandex.ru/watch/33712959" style="position:absolute; left:-9999px;" alt="yandex" /></div></noscript>
 	<!-- /Yandex.Metrika counter -->
+
+
+	<!-- Код тега ремаркетинга Google -->
+	<script type="text/javascript">
+	/* <![CDATA[ */
+	var google_conversion_id = 867216113;
+	var google_custom_params = window.google_tag_params;
+	var google_remarketing_only = true;
+	/* ]]> */
+	</script>
+	<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+	</script>
+	<noscript>
+	<div style="display:inline;">
+	<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/867216113/?guid=ON&amp;script=0"/>
+	</div>
+	</noscript>
+
+
 </body>
 </html>
