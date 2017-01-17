34420749a51e1d535479d836ed4e8c99bcd604dc
Igor Popov <ihptru@gmail.com>
2017-01-17 06:30:13 +0000

    update sms

--- functions.php
+++ functions.php
@@ -1,17 +1,8 @@
 <?php
 
 function send_sms_to_admin($sms_api_id, $sms_admin_phone) {
-	/*$ch = curl_init("http://sms.ru/sms/send");
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-	curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, array(
 
-		"api_id"		=>	$sms_api_id,
-		"to"			=>	$sms_admin_phone,
-		"text"			=>	"Событие+с+сайта+глобал-проект.рф,+проверьте+почту"
-
-	));
-	$body = curl_exec($ch);
-	curl_close($ch);*/
+	$body=file_get_contents("http://sms.ru/sms/send?api_id=".$sms_api_id."&to=".$sms_admin_phone."&text=".urlencode("Событие с глобал-проект.рф, проверьте почту"));
+	return $body;
 }
 ?>
