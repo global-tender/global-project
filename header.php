1eab63540a7039bd995fe3abcecfc39603bfdeb3
ihptru <ihptru@gmail.com>
2017-07-07 20:36:36 +0300

    privacy over

--- header.php
+++ header.php
@@ -36,7 +36,7 @@
 
 		<link rel="stylesheet" href="/css/normalize.css">
 		<link rel="stylesheet" href="/css/main.css">
-		<link rel="stylesheet" href="/css/style.css?ver=0.020">
+		<link rel="stylesheet" href="/css/style.css?ver=0.021">
 
 		<!-- Google -->
 		<script type="text/javascript">
